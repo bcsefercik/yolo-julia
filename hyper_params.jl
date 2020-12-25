9e2e7b5e6124392f5c7b6368c22c9334ba830e7b
Bugra Can Sefercik <bsefercik@gmail.com>
2020-12-26 01:55:15 +0300

    build_targets done.

---
+++ hyper_params.jl
@@ -0,0 +1,20 @@
+IOU_T = 0.20  # iou training threshold
+
+# hyp = {'giou': 3.54,  # giou loss gain
+#        'cls': 37.4,  # cls loss gain
+#        'cls_pw': 1.0,  # cls BCELoss positive_weight
+#        'obj': 64.3,  # obj loss gain (*=img_size/320 if img_size != 320)
+#        'obj_pw': 1.0,  # obj BCELoss positive_weight
+#        'iou_t': 0.20,  # iou training threshold
+#        'lr0': 0.01,  # initial learning rate (SGD=5E-3, Adam=5E-4)
+#        'lrf': 0.0005,  # final learning rate (with cos scheduler)
+#        'momentum': 0.937,  # SGD momentum
+#        'weight_decay': 0.0005,  # optimizer weight decay
+#        'fl_gamma': 0.0,  # focal loss gamma (efficientDet default is gamma=1.5)
+#        'hsv_h': 0.0138,  # image HSV-Hue augmentation (fraction)
+#        'hsv_s': 0.678,  # image HSV-Saturation augmentation (fraction)
+#        'hsv_v': 0.36,  # image HSV-Value augmentation (fraction)
+#        'degrees': 1.98 * 0,  # image rotation (+/- deg)
+#        'translate': 0.05 * 0,  # image translation (+/- fraction)
+#        'scale': 0.05 * 0,  # image scale (+/- gain)
+#        'shear': 0.641 * 0}  # image shear (+/- deg)
--- utils/utils.jl
+++ utils/utils.jl
@@ -1,3 +1,5 @@
+include("../hyper_params.jl")
+
 function build_targets(p, targets, model; atype=Knet.atype())
     # p: yolo_outs
 
@@ -14,26 +16,76 @@ function build_targets(p, targets, model; atype=Knet.atype())
         )
     end
 
-    nt = size(targets_reshaped)[2]
+    targets_reshaped = transpose(targets_reshaped)
+
+    nt = size(targets_reshaped)[1]
     tcls, tbox, indices, anch = [], [], [], []
-    gain = convert(atype, ones(6))
+    gain = convert(atype, ones(1, 6))
 
     for (i, j) in enumerate(model.yolo_layers)
         # i: yolo_layer intra index (1, 2, 3)
         # j: yolo_layer darknet module index
         anchors = model.module_list[j].anchor_vec
-        gain[3:end] .= size(p[i])[[3, 2, 3, 2]]
+        gain[1, 3:end] .= size(p[i])[[3, 2, 3, 2]]  # xyxy gain
         na = size(anchors)[1]
         at = repeat(reshape(1:na, (na, 1)), 1, nt)
 
-        a, t, offsets = [], targets_reshaped .* gain, 0
+        a, t, offsets = [], targets_reshaped .* gain, 0.0
 
         if nt > 0
+            j = wh_iou(anchors, t[:, 5:6]) .> IOU_T
+            tt = repeat(t, na)
+            jj = vcat(j'...)
+            a, t = at'[j'], tt[jj, :]
 
         end
 
-        return targets_reshaped, gain
+        b = Integer.(t[:, 1])
+        c = Integer.(t[:, 2])
+        gxy = t[:, 3:4]  # xy
+        gwh = t[:, 5:6]  # wh
+        gij = ceil.(Integer, gxy .- offsets)  # xy indices starts from 1 in julia
+        gi = gij[:, 1]  # x
+        gj = gij[:, 2]  # y
+
+        push!(
+            indices,
+            (
+                b,
+                a,
+                clamp.(gj, 1, gain[4]),
+                clamp.(gi, 1, gain[3])
+            )
+        )
+
+        push!(
+            tbox,
+            cat(gxy - gij .+ 1.0, gwh; dims=2)  # x (in cell), y (in cell), w, h
+        )
+
+        push!(anch, anchors[a, :])
+        push!(tcls, c)
     end
 
     return tcls, tbox, indices, anch
+end
+
+function wh_iou(wh1, wh2)
+     # Returns the nxm IoU matrix. wh1 is nx2, wh2 is mx2
+    N, _ = size(wh1)
+    M, _ = size(wh2)
+    wh1r = reshape(wh1, (:, 1, 2))  # [N,1,2]
+    wh2r = reshape(wh2, (1, :, 2))  # [1,M,2]
+
+    inter = reshape(
+        prod(min.(wh1r, wh2r), dims=3),
+        (N, M)
+    )  # [N,M]
+
+    uni = reshape(prod(wh1r, dims=3), (:, 1)) .+ reshape(prod(wh2r, dims=3), (1, :))
+    uni = uni .- inter  # [N,M]
+
+    return inter ./ uni
+
+
 end
