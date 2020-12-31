9ecb8c6ea5da24f49a25f494217308d2da234d37
Bugra Can Sefercik <bsefercik@gmail.com>
2021-01-01 00:51:14 +0300

    files restructured

--- utils/constants.jl
+++ utils/constants.jl
@@ -5,14 +5,14 @@ import Images: N0f8, RGB
 
 
 CLASS_MAP = JSON.parsefile(
-    "../class_mappings.json";
+    "../cfg/class_mappings.json";
     dicttype=Dict{String, Integer},
     inttype=Integer,
     use_mmap=true
 )
 
 CLASS_NAMES = JSON.parsefile(
-    "../class_names.json";
+    "../cfg/class_names.json";
     dicttype=Dict{String, String},
     inttype=Integer,
     use_mmap=true
@@ -25,7 +25,7 @@ for (k, v) in CLASS_MAP
     CLASS_NAMES_R[v] = CLASS_NAMES[k]
 end
 
-COLORS = FileIO.load("../colors.jld2")["COLORS"]
+COLORS = FileIO.load("../cfg/colors.jld2")["COLORS"]
 
 # To generate new colors for classes use following line
 # COLORS = [RGB{N0f8}(rand(3)...) for (_, _) in CLASS_NAMES_R]
