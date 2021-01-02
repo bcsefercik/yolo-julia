efb1bf5b470ec91e88c84895277eb5c9f08ea9a4
Bugra Can Sefercik <bsefercik@gmail.com>
2021-01-02 23:20:56 +0300

    map init implementation

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # YOLOv3 - Julia Version w/ Knet
-This repository hosts Julia code for YOLOv3. (Redmon, Joseph, and Ali Farhadi. "Yolov3: An incremental improvement." arXiv preprint arXiv:1804.02767 (2018)). By using code in this repo you can:
+This repository hosts Julia code for YOLOv3.
+
+```
+@article{redmon2018yolov3,
+  title={Yolov3: An incremental improvement},
+  author={Redmon, Joseph and Farhadi, Ali},
+  journal={arXiv preprint arXiv:1804.02767},
+  year={2018}
+}
+```
+
+By using code in this repo you can:
 - Train a Darknet network
 - Use YOLOv3 (Darknet) architecture to detect and classify objects.
 
@@ -50,6 +61,29 @@ loss = darknet(<image_float32_array>, <label_array>; training=false)
 Training tool for YOLOv3. You can see the available training parameter options by running:
 ```
 julia train.jl --help
+
+###
+Output:
+arguments:
+  --model-out MODEL-OUT
+                        Model file path to save the trained model.
+  --results RESULTS     Result file. (default: "results.jld2")
+  --model-config MODEL-CONFIG
+                        Network config file. (default:
+                        "cfg/yolov3.cfg")
+  --preload PRELOAD     Pre-trained model file.
+  --trndata TRNDATA [TRNDATA...]
+                        COCO2014Data files for training.
+  --valdata VALDATA [VALDATA...]
+                        COCO2014Data files for validation.
+  --epoch EPOCH         Number of epochs. (type: Int64, default: 100)
+  --iepoch IEPOCH       Number of instance epochs. (type: Int64,
+                        default: 2)
+  --lr LR               Learning rate (type: Float64, default: 0.001)
+  --period PERIOD       Status printing period. (type: Int64, default:
+                        10)
+  --bs BS               Batch size (type: Int64, default: 8)
+
 ```
 
 ### coco2014.jl
@@ -58,6 +92,12 @@ Cotains all necessary functions to prepare and process coco2014 object detection
 ### nn.jl
 Contains all Knet layer definitions that can be used to build a network from scratch such as Darknet. Layers include YOLOLayers, FeatureFusions, FeatureConcat, etc.
 
+```
+include("nn.jl")
+import .NN
+
+# Start to call NN.<layer> for usage()
+```
 
 ## Visual Tools
 ```
--- utils/map.jl
+++ utils/map.jl
@@ -6,16 +6,170 @@ include("constants.jl")
 include("../models.jl")
 include("../coco2014.jl")
 
-function get_mAP(model::Darknet, data::COCO14Data)
-    MINOVERLAP = 0.5  # default value (defined in the PASCAL VOC2012 challenge)
+# Adopted from: https://medium.com/analytics-vidhya/understanding-the-map-mean-average-precision-evaluation-metric-for-object-detection-432f5cca53b7
+# Faster RCNN version
+
+MINOVERLAP = 0.5  # default value (defined in the PASCAL VOC2012 challenge)
+
+function compute_mAP(preds, labels, iou_thres=MINOVERLAP)
     NUM_CLASSES = length(CLASS_MAP)
 
-    gt_counter_per_class = Dict(Integer, Integer)
+    gt_counter_per_class = Dict{Integer, Integer}()
 
+    gt_classified = Dict()
+    pred_classified = Dict()
 
+    for pred_ins in preds
+        for pred in pred_ins
+            if !haskey(pred_classified, Integer(pred[1]))
+                pred_classified[Integer(pred[1])] = []
+                gt_classified[Integer(pred[1])] = []
+            end
 
-    for (img, y_gt) in data
-        print(size(img))
+            push!(
+                pred_classified[Integer(pred[1])],
+                pred
+            )
+        end
     end
 
+    for label_ins in labels
+        for label in label_ins
+            if !haskey(gt_classified, Integer(label[1]))
+                gt_classified[Integer(label[1])] = []
+                pred_classified[Integer(label[1])] = []
+            end
+
+            push!(
+                gt_classified[Integer(label[1])],
+                label
+            )
+        end
+    end
+
+    sum_AP = 0.0
+    cls_APs = Dict{Integer, Float32}()
+    for (cls, _) in pred_classified
+        cls_APs[cls], _, _ = compute_class_ap(
+            pred_classified[cls],
+            gt_classified[cls],
+            iou_thres
+        )
+
+        sum_AP += cls_APs[cls]
+    end
+
+    mAP = sum_AP / length(cls_APs)
+
+    return mAP, cls_APs
+end
+
+
+function compute_class_ap(predictions_data, labels, iou_thres=MINOVERLAP)
+    nd = length(predictions_data)
+    tp = zeros(nd)
+    fp = zeros(nd)
+
+    sort!(predictions_data, by=o->o[6], rev=true)
+    labels = _get_x1y1x2y2.(labels)
+
+    for (idx, pred) in enumerate(predictions_data)
+        ovmax = -1
+        gt_match = -1
+
+        x1, y1, x2, y2 = pred[2], pred[3], pred[4], pred[5]
+
+        for (idgt, obj) in enumerate(labels)
+            xx1 = max.(x1, obj[2])
+            yy1 = max.(y1, obj[3])
+            xx2 = min.(x2, obj[4])
+            yy2 = min.(y2, obj[5])
+
+            iw = max.(0, xx2 - xx1 .+ 1)
+            ih = max.(0, yy2 - yy1 .+ 1)
+
+
+
+            if iw > 0 && ih > 0
+                uni = (x2 - x1 + 1) * (y2 - y1 + 1) + (obj[4] - obj[2] + 1) * (obj[5] - obj[3] + 1) - iw * ih
+                ov = (iw .* ih) ./ uni
+
+                if ov > ovmax
+                    ovmax = ov
+                    gt_match = idgt
+                end
+            end
+        end
+
+        if ovmax >= iou_thres
+            tp[idx] = 1
+            deleteat!(labels, gt_match)
+        else
+            fp[idx] = 1
+        end
+
+    end
+
+    csum = 0
+    for (idx, val) in enumerate(fp)
+        fp[idx] += csum
+        csum += val
+    end
+
+    csum = 0
+    for (idx, val) in enumerate(tp)
+        tp[idx] += csum
+        csum += val
+    end
+
+    rec = length(labels) > 0 ? tp ./ length(labels) : tp .* 0
+    prec = tp ./ (fp .+ tp)
+
+    ap, mrec, mprec = compute_voc_ap(rec, prec)
+
+    return ap, mrec, mprec
+end
+
+
+function compute_voc_ap(rec, prec)
+
+    if sum(prec) < 1e-5
+        return 0, rec, prec
+    end
+
+    insert!(rec, 1, 0)
+    append!(rec, 1)
+
+    insert!(prec, 1, 0)
+    append!(prec, 1)
+
+    mpre = prec[:]
+    mrec = rec[:]
+
+    for i in length(mpre)-1:-1:1
+       mpre[i] = max.(mpre[i], mpre[i+1])
+    end
+
+    i_list = Array{Integer}([])
+    for i in 2:length(mrec)
+        if mrec[i] != mrec[i-1]
+            append!(i_list, i)
+        end
+    end
+
+    ap = 0.0
+    for i in i_list
+        ap += ((mrec[i] - mrec[i-1]) * mpre[i])
+    end
+
+    return ap, mrec, mpre
+end
+
+function _get_x1y1x2y2(label, img_size=(416, 416))
+    x1 = Integer(round((label[2] - label[4]/2) * img_size[2]))
+    y1 = Integer(round((label[3] - label[5]/2) * img_size[1]))
+    x2 = Integer(round((label[2] + label[4]/2) * img_size[2]))
+    y2 = Integer(round((label[3] + label[5]/2) * img_size[1]))
+
+    return [label[1], x1, y1, x2, y2]
 end
